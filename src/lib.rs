4db1b3924f68227e8d9873eea6be2e08b93d278c
shybyte <shybyte@users.noreply.github.com>
2021-06-08 18:54:54 +0200

    Use local patched version of nlprule

--- src/lib.rs
+++ src/lib.rs
@@ -17,19 +17,21 @@ extern {
 
 #[wasm_bindgen]
 pub fn greet() {
+    utils::set_panic_hook();
+
     let mut tokenizer_bytes: &'static [u8] = include_bytes!("../binaries/en_tokenizer.bin");
     let mut rules_bytes: &'static [u8] = include_bytes!("../binaries/en_rules.bin");
 
     let tokenizer =
         Tokenizer::from_reader(&mut tokenizer_bytes).expect("tokenizer binary is valid");
-    // let rules = Rules::from_reader(&mut rules_bytes).expect("rules binary is valid");
+    let rules = Rules::from_reader(&mut rules_bytes).expect("rules binary is valid");
 
-    // assert_eq!(
-    //     rules.correct("She was not been here since Monday.", &tokenizer),
-    //     String::from("She was not here since Monday.")
-    // );
-    //
-    // let corrected = rules.correct("She was not been here since Monday.", &tokenizer);
+    assert_eq!(
+        rules.correct("She was not been here since Monday.", &tokenizer),
+        String::from("She was not here since Monday.")
+    );
 
-    alert("Hello, nplrule-wasm-example!!");
+    let corrected = rules.correct("She was not been here since Monday.", &tokenizer);
+
+    alert(&corrected);
 }
